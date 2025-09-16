6163ad6da978d3c16967f6b9390d90e4ecaf720d
jyothika <jyothikachitturi5@gmail.com>
2025-09-16 20:07:06 +0530

    ok

--- Backend/index.js
+++ Backend/index.js
@@ -1,34 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
 dotenv.config();
+
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+     .then(() => console.log("Connected to MongoDB"))
+     .catch(err => console.log("Database connection error: ", err))
 
-// const urlSchema = new mongoose.Schema({
-//     originalUrl: String,
-//     shortUrl: String,
-//     clicks: { type: Number, default: 0 },
-// });
-// const Url = mongoose.model('Url', urlSchema);
 
-// app.post('/api/short', async (req, res) => {
-//     try {
-//         const { originalUrl } = req.body;
-//         const shortUrl = nanoid(8);
-//         const Url = new Url({ originalUrl, shortUrl });
-//         await Url.save();
-//         res.status(200).json({message: 'Short URL created successfully', url: Url}); 
-//     }
-//     catch (error) {
-//     }
-// })    
-app.listen(3000, () => { console.log('Server started on port 3000'); });
+app.listen(3000, () => console.log('Server started on port 3000'))
+
+
